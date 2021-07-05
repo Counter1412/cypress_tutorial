bcab63de484782b8e03ef9f111e77a6d71ffcf77
Counter1412 <hoanglong1412@gmail.com>
2021-07-05 18:06:43 +0700

    update git

--- shopee tutorial/cypress/data/locators/homepage.js
+++ shopee tutorial/cypress/data/locators/homepage.js
@@ -6,7 +6,7 @@ module.exports = {
                 background_color: '',
                 color: '',
                 clientLeft: "",
-                clientTop: ""
+               
               }
         },
         
